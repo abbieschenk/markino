24522225205b010cda1dec29cd217d5a4286f092
Abbie Schenk <abbieschenk@gmail.com>
2026-09-06 15:36:14 +0200

    Remove next.js cruft

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   globalIgnores([
-    ".next/**",
     ".astro/**",
     "dist/**",
     "out/**",
     "build/**",
-    "next-env.d.ts",
   ]),
 ]);
 
